49bc65d9d077d153ac0f3974f18b51ddc9739774
TanakAiko <cherifmbaye02@gmail.com>
2026-01-15 17:57:35 +0000

    Add UserRepository interface for user data access with methods to find by username and email

---
+++ user-service/src/main/java/sn/dev/user_service/services/UserRepository.java
@@ -0,0 +1,17 @@
+package sn.dev.user_service.services;
+
+import java.util.Optional;
+
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+
+import sn.dev.user_service.data.User;
+
+@Repository
+public interface UserRepository extends Neo4jRepository<User, String> {
+
+    // Spring generates this query automatically based on the method name!
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
